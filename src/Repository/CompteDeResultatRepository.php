3af85aabd1ca5a2cbc8f76fbc803a078a2471cb0
Léonard Michelet <leonard.michelet@smile.fr>
2019-12-06 19:20:40 +0100

    allow update up to 4.3.* version

--- src/Repository/CompteDeResultatRepository.php
+++ src/Repository/CompteDeResultatRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\CompteDeResultat;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method CompteDeResultat|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class CompteDeResultatRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, CompteDeResultat::class);
     }
--- src/Repository/ShareholdingRepository.php
+++ src/Repository/ShareholdingRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Shareholding;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Shareholding|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ShareholdingRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Shareholding::class);
     }
